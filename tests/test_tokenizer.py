bfb05331c0f87b4d31c0f277837128a36e3bdf87
Matis Codjia <codjiamatis01@email.com>
2026-06-05 15:39:54 +0200

    feat: initialize tokenizer module and test structures

---
+++ tests/test_tokenizer.py
@@ -0,0 +1,18 @@
+from mlfeel.tokenizer import BaseTokenizer
+
+my_tokenizer = BaseTokenizer()
+
+
+def test_html_punctuation():
+    assert my_tokenizer.preprocess_text("Hello,   World! <br/>") == "hello world"
+
+
+def test_spaces_punctuation():
+    assert my_tokenizer.preprocess_text("  NLP is FUN!!!  ") == "nlp is fun"
+
+
+def test_html_mixed_punctuation():
+    assert (
+        my_tokenizer.preprocess_text("<p>U.S. stocks rose 2.3%</p>")
+        == "u s stocks rose 2 3"
+    )
