7903193131cde7a4e692fdd819ed90e628d5c3c8
Matis Codjia <codjiamatis01@email.com>
2026-06-08 23:17:56 +0200

    feat: test for vectorizing

--- src/mlfeel/vectorizer.py
+++ src/mlfeel/vectorizer.py
@@ -1,2 +1,9 @@
 class Vectorizer:
+    dim: int
 
+    def __init__(self):
+        self.dim = 0
+
+    def vectorize(self, words_list: list[str], word_to_index: dict[str, int]):
+        self.dim = len(word_to_index)
+        return [word_to_index[word] for word in words_list]
